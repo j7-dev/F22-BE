113a34ca21e715c0ea095406a49c59fda7c09695
j7 <j7.dev.gg@outlook.com>
2023-08-28 18:07:23 +0000

    feat: add plugins.js

---
+++ config/plugins.js
@@ -0,0 +1,21 @@
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'strapi-provider-email-smtp',
+      providerOptions: {
+        host: 'smtpout.secureserver.net', //SMTP Host
+        port: 465   , //SMTP Port
+        secure: true,
+        username: 'service@yc-tech.co',
+        password: 'X0921565659x+',
+        rejectUnauthorized: true,
+        requireTLS: true,
+        connectionTimeout: 1,
+      },
+    },
+    settings: {
+      defaultFrom: 'service@yc-tech.co',
+      defaultReplyTo: 'service@yc-tech.co',
+    }, 
+  },    
+});
