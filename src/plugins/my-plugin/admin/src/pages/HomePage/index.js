9a387bc2ec87c080aa7de258b9935b85d75488ee
J7 <j7.dev.gg@outlook.com>
2023-08-29 02:30:15 +0800

    test: test git workflow

--- src/plugins/my-plugin/admin/src/pages/HomePage/index.js
+++ src/plugins/my-plugin/admin/src/pages/HomePage/index.js
@@ -11,7 +11,7 @@ import pluginId from "../../pluginId";
 const HomePage = () => {
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage test</h1>
+      <h1>{pluginId}&apos;s HomePage test test</h1>
       <p>Happy coding</p>
     </div>
   );
