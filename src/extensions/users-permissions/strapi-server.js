3d35f9dadbec8f968544b9bfec3033982612812c
J7 <j7.dev.gg@outlook.com>
2023-09-17 03:40:07 +0800

    feat: remove unused mock use api

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,16 +1,4 @@
-const { faker } = require('@faker-js/faker')
 const user = require('./content-types/user')
-
-const getUserName = (role) => {
-  switch (role) {
-    case 1:
-      return 'user'
-    case 3:
-      return 'agent'
-    default:
-      return 'user'
-  }
-}
 
 module.exports = (plugin) => {
   plugin.contentTypes.role.schema.pluginOptions = {
@@ -22,66 +10,15 @@ module.exports = (plugin) => {
     },
   }
 
-  plugin.controllers.user.mock = async (ctx) => {
-    try {
-      const qty = ctx.request.body?.qty || 10
-      const role = ctx.request.body?.role || 2
-      // 1 = authenticated, 2 = public, 3 = agent
-
-      const latestUser = await strapi.db
-        .query('plugin::users-permissions.user')
-        .findOne({
-          select: ['id'],
-          orderBy: { id: 'DESC' },
-        })
-
-      const latestUserId = latestUser?.id || null
-
-      const userName = getUserName(role)
-      for (let i = 0; i < qty; i++) {
-        const createResult = await strapi.entityService.create(
-          'plugin::users-permissions.user',
-          {
-            data: {
-              username: `${userName}_${latestUserId + i}`,
-              email: `${userName}_${latestUserId + i}@gmail.com`,
-              password: '123456',
-              confirmed: true,
-              role,
-              blocked: false,
-              cash_balance: 0,
-              reward_point_balance: 0,
-              display_name: faker.person.fullName(),
-              phone: faker.phone.number('0#-####-####'),
-              gender: faker.helpers.arrayElement(['MALE', 'FEMALE']),
-              birthday: faker.date.birthdate({ min: 18, max: 55, mode: 'age' }),
-            },
-          }
-        )
-        console.log(
-          '⭐  plugin.controllers.user.mock=  createResult',
-          createResult
-        )
+  plugin.routes['content-api'].routes = plugin.routes['content-api'].routes.map(
+    (item) => {
+      if (item.method == 'POST' && item.path == '/auth/local') {
+        item.config.middlewares = ['global::loginDetail']
       }
 
-      ctx.body = {
-        status: '200',
-        message: `✅ mock ${qty} users success`,
-        data: {},
-      }
-    } catch (err) {
-      ctx.body = err
+      return item
     }
-  }
-
-  plugin.routes['content-api'].routes.push({
-    method: 'POST',
-    path: '/user/mock',
-    handler: 'user.mock',
-    config: {
-      prefix: '',
-    },
-  })
+  )
 
   plugin.contentTypes.user = user
 
