05565892990fddb0d653dfea372f10a28308e150
J7 <j7.dev.gg@outlook.com>
2023-09-18 12:21:52 +0800

    feat: add pp tables

---
+++ src/api/pp-transaction/routes/pp-transaction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pp-transaction router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::pp-transaction.pp-transaction');
