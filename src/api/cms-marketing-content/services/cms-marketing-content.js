057545ad88ef80f6b1b13721cf0b4d27422dcd72
J7 <j7.dev.gg@outlook.com>
2023-09-19 23:20:24 +0800

    feat: add CMS post and Marketing Content

---
+++ src/api/cms-marketing-content/services/cms-marketing-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-marketing-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::cms-marketing-content.cms-marketing-content');
