1612c3b922b44f730be72b3dc348e7378d7fa291
J7 <j7.dev.gg@outlook.com>
2023-09-19 13:58:24 +0800

    feat: evo ip test

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,11 +2,10 @@ module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
