31bca2c2d43bdc853a106a23645f7b25fb498698
J7 <j7.dev.gg@outlook.com>
2023-09-19 16:21:25 +0800

    refactor: PP getCasinoGames calculate hash

--- src/api/pp-gp/controllers/pp-gp.js
+++ src/api/pp-gp/controllers/pp-gp.js
@@ -1,6 +1,7 @@
 'use strict'
 const axios = require('axios').default
 const _ = require('lodash')
+const crypto = require('crypto')
 /**
  * A set of functions called "actions" for `pp-gp`
  */
@@ -9,7 +10,20 @@ module.exports = {
   getCasinoGames: async (ctx, next) => {
     const apiUrl = process?.env?.PP_OPEN_GAME_API_URL
     const secureLogin = process?.env?.PP_SECURE_LOGIN
-    const hash = process?.env?.PP_HASH
+
+    // 要加密的原始字符串
+    const originalString = `secureLogin=${secureLogin}`
+    // 您的密鑰
+    const secretKey = process?.env?.PP_SECRET_KEY
+    // 將原始字符串和密鑰結合
+    const stringToHash = originalString + secretKey
+    // 建立 MD5 加密器
+    const md5Hash = crypto.createHash('md5')
+    // 更新加密器的內容
+    md5Hash.update(stringToHash)
+    // 進行加密並取得加密後的十六進位字串
+    const hash = md5Hash.digest('hex')
+
     const queryParams = {
       secureLogin,
       hash,
