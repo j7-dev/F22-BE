db0f3fdad7f6b7e73c6eede498125a089a1e9dea
J7 <j7.dev.gg@outlook.com>
2023-09-19 23:20:24 +0800

    feat: add CMS post and Marketing Content
    (cherry picked from commit 057545ad88ef80f6b1b13721cf0b4d27422dcd72)

---
+++ src/api/cms-marketing-content/controllers/cms-marketing-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-marketing-content controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cms-marketing-content.cms-marketing-content');
