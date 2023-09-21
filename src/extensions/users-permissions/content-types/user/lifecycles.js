e8d1ec79b0f49e1dc8f9752bb3c940650b69052a
J7 <j7.dev.gg@outlook.com>
2023-09-21 18:46:40 +0800

    refactor: user uuid and update display_name after create

--- src/extensions/users-permissions/content-types/user/lifecycles.js
+++ src/extensions/users-permissions/content-types/user/lifecycles.js
@@ -1,11 +1,23 @@
+const { nanoid } = require('nanoid')
+
 module.exports = {
+  async beforeCreate(event) {
+    const { result, params } = event
+    const data = params?.data
+    data.uuid = nanoid()
+    if (!data?.display_name) {
+      data.display_name = data.username
+    }
+  },
   async afterCreate(event) {
     const { result } = event
     const created_user_id = result?.id
 
-    const currencies = await strapi.entityService.findMany(
-      'api::currency.currency'
+    // 取得支援的幣別
+    const siteSetting = await strapi.entityService.findMany(
+      'api::site-setting.site-setting'
     )
+    const supportCurrencies = siteSetting?.support_currency
 
     // 取得全 amount type id
     const amountTypeIds = await strapi.entityService.findMany(
@@ -15,8 +27,7 @@ module.exports = {
       }
     )
 
-    currencies.forEach(async (currency) => {
-      const { id: currency_id } = currency
+    supportCurrencies.forEach(async (currency) => {
       amountTypeIds.forEach(async (amountType) => {
         const { id: amount_type_id } = amountType
         const createResult = await strapi.entityService.create(
@@ -25,7 +36,7 @@ module.exports = {
             data: {
               amount: 0,
               user: created_user_id,
-              currency: currency_id,
+              currency,
               amount_type: amount_type_id,
             },
           }
