dbf0555a2517c24406476c98ab3b06ab17b58c2f
J7 <j7.dev.gg@outlook.com>
2023-09-25 14:42:52 +0800

    feat: remove unused plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,25 +1,21 @@
 module.exports = ({ env }) => ({
   email: {
     config: {
-      provider: "strapi-provider-email-smtp",
+      provider: 'strapi-provider-email-smtp',
       providerOptions: {
-        host: "smtpout.secureserver.net", //SMTP Host
+        host: 'smtpout.secureserver.net', //SMTP Host
         port: 465, //SMTP Port
         secure: true,
-        username: "service@yc-tech.co",
-        password: "X0921565659x+",
+        username: 'service@yc-tech.co',
+        password: 'X0921565659x+',
         rejectUnauthorized: true,
         requireTLS: true,
         connectionTimeout: 1,
       },
     },
     settings: {
-      defaultFrom: "service@yc-tech.co",
-      defaultReplyTo: "service@yc-tech.co",
+      defaultFrom: 'service@yc-tech.co',
+      defaultReplyTo: 'service@yc-tech.co',
     },
   },
-  "my-plugin": {
-    enabled: true,
-    resolve: "./src/plugins/my-plugin",
-  },
-});
+})
