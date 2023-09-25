f2aca7ff55f8be626da4c9f9b01431029f3b5b84
J7 <j7.dev.gg@outlook.com>
2023-09-25 13:33:04 +0800

    feat: add error logger

--- src/api/wallet-api/controllers/balance.js
+++ src/api/wallet-api/controllers/balance.js
@@ -3,33 +3,22 @@
 module.exports = {
   async add(ctx) {
     const body = ctx.request.body
-    try {
-      const result = await strapi
-        .service('api::wallet-api.wallet-api')
-        .add(body)
-      // respond
-      ctx.body = {
-        status: '200',
-        message: 'updateBalance success',
-        data: result,
-      }
-    } catch (err) {
-      ctx.body = err
+
+    const result = await strapi.service('api::wallet-api.wallet-api').add(body)
+    // respond
+    ctx.body = {
+      status: '200',
+      message: 'updateBalance success',
+      data: result,
     }
   },
   async get(ctx) {
     const query = ctx.request.query
-    try {
-      const result = await strapi
-        .service('api::wallet-api.wallet-api')
-        .get(query)
-      ctx.body = {
-        status: '200',
-        message: 'get cash_balance success',
-        data: result,
-      }
-    } catch (err) {
-      ctx.body = err
+    const result = await strapi.service('api::wallet-api.wallet-api').get(query)
+    ctx.body = {
+      status: '200',
+      message: 'get cash_balance success',
+      data: result,
     }
   },
 }
---
+++ src/middlewares/error-logger.js
@@ -0,0 +1,61 @@
+const winston = require('winston')
+require('winston-daily-rotate-file')
+const path = require('path')
+
+const addTimestamp = winston.format((info, opts) => {
+  const newInfo = opts.timestamp
+    ? {
+        timestamp: new Date().toISOString(),
+        ...info,
+      }
+    : info
+
+  return newInfo
+})
+
+const logger = winston.createLogger({
+  // 當 transport 不指定 level 時 , 使用 info 等級
+  level: 'info',
+  // 設定輸出格式
+  // format: winston.format.json(),
+  format: winston.format.combine(
+    addTimestamp({ timestamp: true }), // 使用自定义格式化器添加时间戳
+    winston.format.json() // 保留原有的 JSON 格式
+  ),
+  // 設定此 logger 的日誌輸出器
+  transports: [
+    // 只有 error 等級的錯誤 , 才會將訊息寫到 error.log 檔案中
+    new winston.transports.DailyRotateFile({
+      level: 'error',
+      filename: path.join(__dirname, '../..', 'logs', `error-%DATE%.log`),
+      datePattern: 'YYYY-MM-DD',
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+    // info or 以上的等級的訊息 , 將訊息寫入 combined.log 檔案中
+    new winston.transports.DailyRotateFile({
+      filename: path.join(__dirname, '../..', 'logs', `combined-%DATE%.log`),
+      datePattern: 'YYYY-MM-DD',
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+module.exports = (config, { strapi }) => {
+  return async (ctx, next) => {
+    try {
+      // 让请求继续处理
+      await next()
+    } catch (error) {
+      // 如果发生错误，使用 logger.error 记录错误消息
+      logger.error('API Error:', error)
+
+      // 将错误消息发送给客户端
+      ctx.status = error.status || 500
+      ctx.body = {
+        error: error?.message || 'Internal Server Error22',
+      }
+    }
+  }
+}
