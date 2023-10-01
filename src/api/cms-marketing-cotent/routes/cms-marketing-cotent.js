380680d9a578171c4ed1168a7a8fa392e587534b
J7 <j7.dev.gg@outlook.com>
2023-10-01 13:34:56 +0800

    feat: add CMS post & CMS Marketing Content

---
+++ src/api/cms-marketing-cotent/routes/cms-marketing-cotent.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cms-marketing-cotent router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cms-marketing-cotent.cms-marketing-cotent');
