e6059fa23b8834324c80662ddee9143fc2262098
J7 <j7.dev.gg@outlook.com>
2023-10-03 12:37:43 +0800

    feat: add lifecycle if withdraw is Approved

--- src/api/transaction-record/content-types/transaction-record/lifecycles.js
+++ src/api/transaction-record/content-types/transaction-record/lifecycles.js
@@ -1,52 +1,57 @@
 module.exports = {
   async beforeUpdate(event) {
     const { params } = event
-    let { populate } = params
-    if (populate?.user) {
-      populate.user = {
-        select: ['id', 'username'],
-      }
-    } else {
-      populate = {
-        user: {
-          select: ['id', 'username'],
+    const { data, where } = params
+    const theTxn = await strapi.entityService.findOne(
+      'api::transaction-record.transaction-record',
+      where?.id,
+      {
+        populate: {
+          user: {
+            fields: ['id'],
+          },
+          updated_by_user_id: {
+            fields: ['id', 'display_name'],
+          },
         },
       }
-    }
-  },
-  async afterUpdate(event) {
-    const { params, result } = event
-    const { data } = params
+    )
 
     const status = data?.status
-    const type = data?.type
+    const type = theTxn?.type
+
     if (type === 'WITHDRAW' && status === 'SUCCESS') {
-      const user_id = result?.user?.id
+      const user_id = theTxn?.user?.id
       if (!user_id) {
         throw new Error('user_id not found')
+      }
+      if (theTxn?.updated_by_user_id?.id) {
+        throw new Error(
+          `Can't Update, this record has been updated by user ${theTxn?.updated_by_user_id?.display_name}`
+        )
       }
 
       // 提款已核准，扣除 balance
       const updateResult = await strapi
         .service('api::wallet-api.wallet-api')
         .addBalance({
-          amount: data?.amount,
-          currency: data?.currency,
-          amount_type: data?.amount_type,
+          amount: theTxn?.amount,
+          currency: theTxn?.currency,
+          amount_type: theTxn?.amount_type,
           user_id,
         })
 
       // 扣款成功  更新 balance_after_mutate
       if (updateResult?.id) {
-        const updateTxnResult = await strapi.entityService.update(
-          'api::transaction-record.transaction-record',
-          result?.id,
-          {
-            data: {
-              balance_after_mutate: updateResult?.amount,
-            },
-          }
-        )
+        data.status = 'SUCCESS'
+        data.balance_after_mutate = updateResult?.amount
+      } else {
+        data.status = 'FAILED'
+        data.balance_after_mutate = null
+        data.title = '提款失敗'
+        data.description = JSON.stringify(updateResult)
+
+        throw new Error(`update balance failed ${JSON.stringify(updateResult)}`)
       }
     }
   },
