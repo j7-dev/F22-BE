ba5872c0d8c7f869e8eb42f5fedbb46920979cc8
J7 <j7.dev.gg@outlook.com>
2023-10-08 13:23:10 +0800

    feat: test for pp opengame , add lobbyUrl

--- src/api/pp-gp/controllers/pp-gp.js
+++ src/api/pp-gp/controllers/pp-gp.js
@@ -95,7 +95,7 @@ module.exports = {
       ...query,
       secureLogin,
       token,
-      lobbyUrl: 'https://f22-fe.vercel.app/slot', // TODO
+      lobbyUrl: 'https://f22-fe.vercel.app/slot', // TODO test for pp
     }
     const hash = getHash(params)
     const queryString = objectToQueryString({
