118f70a7d8c935ceac96d1afb9ff2fdb4238e28a
J7 <j7.dev.gg@outlook.com>
2023-10-22 19:59:05 +0800

    feat: CORS test

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
     config: {
       enabled: true,
       header: '*',
-      origin: ['be-dev.smtbet7.com', 'smtbet7.com'],
+      origin: ['be-dev.smtbet7.com', 'smtbet7.com', 'www.w3schools.com'],
     },
   },
   'strapi::logger',
