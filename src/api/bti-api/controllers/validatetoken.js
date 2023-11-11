95b5b0b4336d5f7ccccb0c70f4fd73a2418715ef
J7 <j7.dev.gg@outlook.com>
2023-11-11 15:53:54 +0800

    fix: [Kevin][BTI]BTI的最小投注金額從810改成5,000 #95

--- src/api/bti-api/controllers/validatetoken.js
+++ src/api/bti-api/controllers/validatetoken.js
@@ -5,9 +5,10 @@
  */
 
 module.exports = {
+  // TODO
   validatetoken: async (ctx, next) => {
-    ctx.type = 'text/plain';
-    var userInfo;
+    ctx.type = 'text/plain'
+    var userInfo
 
     try {
       // 取的 query string 的 auth_token
@@ -17,12 +18,12 @@ module.exports = {
         ctx.body = formatAsKeyValueText({
           error_code: '-3',
           error_message: 'TokenNotValid',
-        });
+        })
         return
       }
 
       //get user info by token
-      const token = auth_token;
+      const token = auth_token
       const userInfoArray = await strapi.entityService.findMany(
         'api::bti-token-info.bti-token-info',
         {
@@ -32,64 +33,85 @@ module.exports = {
           populate: ['user_id'],
           sort: { createdAt: 'desc' },
         }
-      );
+      )
 
-      userInfo = userInfoArray[0];
+      userInfo = userInfoArray[0]
 
       if (userInfo == undefined) {
         ctx.body = formatAsKeyValueText({
           error_code: '-3',
           error_message: 'TokenNotValid',
-        });
+        })
         return
       }
 
       //get balance by user id
       const userBalanceArray = await strapi
         .service('api::wallet-api.wallet-api')
-        .get({user_id: userInfo.user_id.id});
+        .get({ user_id: userInfo.user_id.id })
 
-      const userBalance = userBalanceArray[0];
+      const userBalance = userBalanceArray[0]
 
       if (userBalance == undefined) {
         ctx.body = formatAsKeyValueText({
           error_code: '-3',
           error_message: 'TokenNotValid',
-        });
+        })
         return
       }
+      const cust_id = userInfo?.user_id?.id
+      const cust_login = userInfo?.user_id?.username
+      const city = 'KR'
+      const country = 'KR'
+      const currency_code = userInfo?.currency
+      const language = 'ko'
+
+      /**
+       * EXAMPLE
+       * error_code=0
+       * error_message=OK
+       * cust_id=bti_derek_cn
+       * cust_login=bti_derek_cn
+       * city=city
+       * country=CN
+       * currency_code=CNY
+       * balance=53549.2690
+       * extSessionID=bti_derek_cn-104.199.162.249-20231111073055
+       * data=<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><status_code>00</status_code><member_id>testubc</member_id><member_name>testubc</member_name><city>city</city><country>CN</country><currency>uBC</currency><language>zh</language><Sport MinBet="0.01" MaxBet="999999999"><Branch ID="2" MinBet="200.00" MaxBet="2000.00" MaxBetPerMatch="10.00"/></Sport></s:Envelope>
+       */
 
       ctx.body = formatAsKeyValueText({
         error_code: '0',
         error_message: 'No Error',
-        cust_id: userInfo.user_id.id,
+        cust_id,
         balance: parseFloat(userBalance.amount),
-        cust_login: userInfo.user_id.username,
-        city: 'KR',
-        country: 'KR',
-        currency_code: userInfo.currency,
-      });
+        cust_login,
+        city,
+        country,
+        currency_code,
+        data: `<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><status_code>00</status_code><member_id>${cust_id}</member_id><member_name>${cust_login}</member_name><city>${city}</city><country>${country}</country><currency>${currency_code}</currency><language>${language}</language><Sport MinBet="5000" MaxBet="999999999"><Branch ID="2" MinBet="5000" MaxBet="999999999" MaxBetPerMatch="10.00"/></Sport></s:Envelope>`,
+      })
     } catch (err) {
       ctx.body = formatAsKeyValueText({
         error_code: '-1',
         error_message: 'GeneralError',
-        err: err
-      });
+        err: err,
+      })
       return
     }
-  }
+  },
 }
 
 function formatAsKeyValueText(data) {
-  let plainText = '';
-  let isFirstLine = true;
+  let plainText = ''
+  let isFirstLine = true
 
   for (const key in data) {
     if (!isFirstLine) {
-      plainText += '\n'; // Add newline if it's not the first line
+      plainText += '\n' // Add newline if it's not the first line
     }
-    plainText += `${key}=${data[key]}`;
-    isFirstLine = false;
+    plainText += `${key}=${data[key]}`
+    isFirstLine = false
   }
-  return plainText;
+  return plainText
 }
