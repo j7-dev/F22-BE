b70b070856da19be5804bc9a5351a14a494e6241
J7 <j7.dev.gg@outlook.com>
2023-11-13 03:18:54 +0800

    fix: test BTI

--- src/api/bti-api/controllers/commitreserve.js
+++ src/api/bti-api/controllers/commitreserve.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict'
 
 /**
  * A set of functions called "actions" for `bti-api`
@@ -6,63 +6,72 @@
 
 module.exports = {
   commitreserve: async (ctx, next) => {
-    ctx.type = 'text/plain';
+    ctx.type = 'text/plain'
     try {
       // 取的 query string 的 auth_token
-      const { cust_id, reserve_id, agent_id, customer_id, purchase_id } = ctx.request.query;
+      const { cust_id, reserve_id, agent_id, customer_id, purchase_id } =
+        ctx.request.query
 
       // 取得 reserve_id 的所有 record
       const reserve = await strapi.entityService.findMany(
-        "api::bti-requests-singular.bti-requests-singular",
+        'api::bti-requests-singular.bti-requests-singular',
         {
-          fields: ["id", "trx_id", "cust_id", "amount", "type", "reserve_id", "req_id", "after_balance"],
+          fields: [
+            'id',
+            'trx_id',
+            'cust_id',
+            'amount',
+            'type',
+            'reserve_id',
+            'req_id',
+            'after_balance',
+          ],
           filters: { reserve_id },
         }
-      );
-      console.log(reserve);
-      var current_balance = 0;
+      )
+      console.log(reserve)
+      var current_balance = 0
       try {
         const result = await strapi
           .service('api::wallet-api.wallet-api')
           .get({ user_id: cust_id })
 
-        current_balance = parseFloat(result[0].amount);
+        current_balance = parseFloat(result[0].amount)
       } catch (err) {
-        return;
+        return
       }
 
       if (reserve.length == 0) {
         ctx.body = formatAsKeyValueText({
-          error_code: "0",
-          error_message: "ReserveID Not Exist",
-          balance: current_balance
-        });
-        return;
+          error_code: '0',
+          error_message: 'ReserveID Not Exist',
+          balance: current_balance,
+        })
+        return
       }
 
-      var accepted_reserve_amount = 0;
-      var accepted_debitreserve_amount = 0;
-      var available_amount = 0;
+      var accepted_reserve_amount = 0
+      var accepted_debitreserve_amount = 0
+      var available_amount = 0
 
       for (const item of reserve) {
-        if (item.type === "reserve") {
-          accepted_reserve_amount += item.amount;
+        if (item.type === 'reserve') {
+          accepted_reserve_amount += item.amount
         }
 
-        if (item.type === "debitreserve") {
-          accepted_debitreserve_amount += item.amount;
+        if (item.type === 'debitreserve') {
+          accepted_debitreserve_amount += item.amount
         }
 
-        if (item.type === "commitreserve") {
+        if (item.type === 'commitreserve') {
           ctx.body = formatAsKeyValueText({
-            error_code: "0",
-            error_message: "No Error",
-            balance: current_balance
-          });
-          return;
+            error_code: '0',
+            error_message: 'No Error',
+            balance: current_balance,
+          })
+          return
         }
       }
-
 
       const result = await strapi.entityService.create(
         'api::bti-requests-singular.bti-requests-singular',
@@ -75,64 +84,66 @@ module.exports = {
             customer_id: customer_id,
             purchase_id: purchase_id,
             url: ctx.request.url,
-            type: "commitreserve",
+            type: 'commitreserve',
           },
         }
       )
-      available_amount = accepted_reserve_amount - accepted_debitreserve_amount;
+      available_amount = accepted_reserve_amount - accepted_debitreserve_amount
 
       //add available_amount back to player wallet
-      console.log("commitreserve - start update balance");
+      console.log('commitreserve - start update balance')
       const body = {
         user_id: cust_id,
         amount: available_amount,
         title: 'bti-commitreserve',
-        type: 'CREDIT',
+        type: 'BET',
         by: 'bti-api',
         currency: 'KRW',
-        ref_id:reserve_id
+        ref_id: reserve_id,
       }
 
-      var krw_amount = 0;
+      var krw_amount = 0
       try {
-        const update_balance_result = await strapi.service('api::wallet-api.wallet-api').add(body)
+        const update_balance_result = await strapi
+          .service('api::wallet-api.wallet-api')
+          .add(body)
 
         for (const balance of update_balance_result.balances) {
-          if (balance.currency = "KRW") {
-            krw_amount = balance.amount;
+          if ((balance.currency = 'KRW')) {
+            krw_amount = balance.amount
           }
         }
       } catch (err) {
         ctx.body = formatAsKeyValueText({
-          error_code: "-4",
-          error_message: "Insufficient Amount",
-          balance: current_balance
-        });
-        return;
+          error_code: '-4',
+          error_message: 'Insufficient Amount',
+          balance: current_balance,
+        })
+        return
       }
-      console.log("commitreserve - end update balance");
+      console.log('commitreserve - end update balance')
 
       ctx.body = formatAsKeyValueText({
-        error_code: "0",
-        error_message: "No Error",
-        balance: krw_amount
-      });
+        error_code: '0',
+        error_message: 'No Error',
+        balance: krw_amount,
+      })
     } catch (err) {
-      ctx.body = err;
+      ctx.body = err
     }
   },
-};
+}
 
 function formatAsKeyValueText(data) {
-  let plainText = '';
-  let isFirstLine = true;
+  let plainText = ''
+  let isFirstLine = true
 
   for (const key in data) {
     if (!isFirstLine) {
-      plainText += '\n'; // Add newline if it's not the first line
+      plainText += '\n' // Add newline if it's not the first line
     }
-    plainText += `${key}=${data[key]}`;
-    isFirstLine = false;
+    plainText += `${key}=${data[key]}`
+    isFirstLine = false
   }
-  return plainText;
+  return plainText
 }
