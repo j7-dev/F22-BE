a1eac7a8b46007dd1d3d95b877d0f174ae42ca49
J7 <j7.dev.gg@outlook.com>
2023-11-12 10:02:20 +0800

    feat: 新增 coupon 領取時間判斷

--- src/api/coupon/controllers/coupon.js
+++ src/api/coupon/controllers/coupon.js
@@ -1,5 +1,7 @@
 'use strict'
-
+const dayjs = require('dayjs')
+const isBetween = require('dayjs/plugin/isBetween')
+dayjs.extend(isBetween)
 /**
  * coupon controller
  */
@@ -28,18 +30,66 @@ module.exports = createCoreController('api::coupon.coupon', {
       'api::coupon.coupon',
       coupon_id,
       {
-        fields: ['id'],
+        fields: ['id', 'is_claimed'],
         populate: {
           user: {
             fields: ['id'],
           },
+          period: {
+            fields: '*',
+          },
         },
       }
     )
+    const start_datetime = coupon?.period?.start_datetime
+    const end_datetime = coupon?.period?.end_datetime
+    const is_before = dayjs().isBefore(start_datetime)
+    const is_after = dayjs().isAfter(end_datetime)
+    console.log('⭐  obj:', {
+      start_datetime,
+      end_datetime,
+      now: dayjs().format('YYYY-MM-DD HH:mm:ss.SSSSSS'),
+    })
+
+    console.log(
+      '⭐  isBetween:',
+      dayjs().isBetween(start_datetime, end_datetime)
+    )
+
+    // const startTime = start_datetime ? dayjs(start_datetime) : null
+    // const endTime = end_datetime ? dayjs(end_datetime) : null
+
+    // 1. 是否是這個用戶的?
+    // 2. 是否已經被領取
+    // 3. 是否在時間內，可被領取
 
     // 檢查要領取的用戶是否與這優惠券發放對象一樣
     if (coupon?.user?.id !== user_id) {
       return ctx.badRequest(`coupon_id is not for this user`)
+    }
+
+    if (coupon?.is_claimed) {
+      return ctx.badRequest(`this coupon_id is already claimed`)
+    }
+
+    // 如果沒有設end time 或是在時間內
+
+    if (
+      start_datetime &&
+      end_datetime &&
+      !dayjs().isBetween(start_datetime, end_datetime)
+    ) {
+      return ctx.badRequest(`this coupon_id is not in period`)
+    }
+    if (start_datetime && !end_datetime) {
+      if (is_before) {
+        return ctx.badRequest(`this coupon_id can't be claimed yet`)
+      }
+    }
+    if (!start_datetime && end_datetime) {
+      if (is_after) {
+        return ctx.badRequest(`this coupon_id is expired, can't claimed`)
+      }
     }
 
     const updateResult = await strapi.entityService.update(
